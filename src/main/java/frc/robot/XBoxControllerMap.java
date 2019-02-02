8d54e0f4b7c27e299fd921f9d18726a8cb8f6fc3
Brian-Thomas Rogers <broge2@uis.edu>
2019-02-02 14:23:04 -0600

    Add XboxControllerMap.java

---
+++ src/main/java/frc/robot/XBoxControllerMap.java
@@ -0,0 +1,37 @@
+package frc.robot;
+
+public interface XBoxControllerMap {
+	//Digital Buttons
+	public static final int A = 1;
+	public static final int B = 2;
+	public static final int X = 3;
+	public static final int Y = 4;
+	
+	public static final int LB = 5;
+	public static final int RB = 6;
+	
+	public static final int L3 = 9;
+	public static final int R3 = 10;
+	public static final int BACK = 7;
+	public static final int START = 8;
+	//*******************************//
+	
+	//Analog Controls
+	public static final int LeftJoystickX = 0;
+	public static final int LeftJoystickY = 1;
+	public static final int LT = 2;
+	public static final int RT = 3;
+	public static final int RightJoystickX = 4;
+	public static final int RightJoystickY = 5;
+	
+	//POV Directions
+	public static final int NORTH = 0;
+	public static final int NORTHEAST = 45;
+	public static final int EAST = 90;
+	public static final int SOUTHEAST = 135;
+	public static final int SOUTH = 180;
+	public static final int SOUTHWEST = 225;
+	public static final int WEST = 270;
+	public static final int NORTHWEST = 315;
+	
+}
