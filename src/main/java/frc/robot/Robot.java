2f78e47589ac3399f50bfd24befc50d9e55d328f
Brian-Thomas Rogers <broge2@uis.edu>
2019-02-02 14:47:27 -0600

    Add code for drive and drive subsystem
    Had to modify Robot.java to include the subsystem.  Not tested.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,7 +12,10 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.Drive;
+import frc.robot.subsystems.DriveSubsystem;
+// import frc.robot.commands.ExampleCommand;
+// import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
 
 /**
@@ -24,6 +27,7 @@ import frc.robot.subsystems.ExampleSubsystem;
  */
 public class Robot extends TimedRobot {
   public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
+  public static DriveSubsystem driveSubsystem = new DriveSubsystem();
   public static OI m_oi;
 
   Command m_autonomousCommand;
@@ -36,7 +40,7 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     m_oi = new OI();
-    m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
+    // m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
     // chooser.addOption("My Auto", new MyAutoCommand());
     SmartDashboard.putData("Auto mode", m_chooser);
   }
