459c26182271577c6d38c0a312d5970882c0a27a
Brian-Thomas Rogers <broge2@uis.edu>
2019-02-04 19:02:36 -0600

    Start  writing grip subsystem

---
+++ src/main/java/frc/robot/subsystems/GripSubsystem.java
@@ -0,0 +1,79 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.Timer;
+
+
+public class GripSubsystem extends Subsystem {
+
+    private DoubleSolenoid leftGripSol;
+    private int leftGripSolForwardChannel;
+    private int leftGripSolBackwardChannel;
+
+    private DoubleSolenoid rightGripSol;
+    private int rightGripSolForwardChannel;
+    private int rightGripSolBackwardChannel;
+
+    private boolean isSqueezed;
+
+    private GripSubsystem gripInstance;
+
+    public GripSubsystem() {
+        if(gripInstance == null){
+            gripInstance = this;
+        }
+        leftGripSol = new DoubleSolenoid(leftGripSolForwardChannel, leftGripSolBackwardChannel);
+        rightGripSol = new DoubleSolenoid(rightGripSolForwardChannel, rightGripSolBackwardChannel);
+
+        isSqueezed = false;
+    }
+
+    @Override
+    public void initDefaultCommand() {
+        
+    }
+
+    @Override
+    public void periodic() {
+        // Put code here to be run every loop
+
+    }
+
+    public boolean isSqueezing(){
+        return isSqueezed;
+    }
+
+    public void squeeze(){
+        if(!isSqueezed){
+            isSqueezed = true;
+            (new Thread(() -> {
+                    stop();
+                    leftGripSol.set(Value.kForward);
+                    rightGripSol.set(Value.kForward);
+                    Timer.delay(1);
+                    stop();
+            })).start();
+        }
+    }
+
+    public void unsqueeze(){
+        if(isSqueezed){
+            isSqueezed = false;
+            (new Thread(() -> {
+                    stop();
+                    leftGripSol.set(Value.kReverse);
+                    rightGripSol.set(Value.kReverse);
+                    Timer.delay(1);
+                    stop();
+            })).start();
+        }
+    }
+
+    public void stop(){
+        leftGripSol.set(Value.kOff);
+        rightGripSol.set(Value.kOff);
+    }
+}
