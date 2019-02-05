e6336b58a47b52df9c50086a958acc6517ba6694
Joshua Smith <jsmit6@uis.edu>
2019-02-04 18:33:28 -0600

    Creation of the ArmSubsystem

---
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -0,0 +1,35 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import frc.robot.commands.Arm;
+
+public class ArmSubsystem extends Subsystem {
+
+    private CANSparkMax liftMotor;
+    public ArmSubsystem() {
+        liftMotor = new CANSparkMax(5, MotorType.kBrushless);
+        liftMotor.setInverted(false);
+    }
+
+    @Override
+    public void initDefaultCommand() {
+        setDefaultCommand(new Arm());
+    }
+
+    @Override
+    public void periodic() {
+        // Put code here to be run every loop
+
+    }
+
+    public void lift(double rTrigger, double lTrigger){
+        liftMotor.set(rTrigger - lTrigger);
+    }
+
+    public void stop(){
+        liftMotor.stopMotor();
+    }
+}
