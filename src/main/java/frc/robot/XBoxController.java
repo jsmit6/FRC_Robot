d2b1d132ab259812bc204c368e81a9cd8f5b77ce
Brian-Thomas Rogers <broge2@uis.edu>
2019-03-14 01:15:03 -0500

    Change dead_zone to deadZone to keep camel case

--- src/main/java/frc/robot/XBoxController.java
+++ src/main/java/frc/robot/XBoxController.java
@@ -4,33 +4,33 @@ import edu.wpi.first.wpilibj.Joystick;
 import frc.robot.XBoxControllerMap;
 
 public class XBoxController {
-    public static double leftJoystickY(Joystick controller, double dead_zone){
+    public static double leftJoystickY(Joystick controller, double deadZone){
         double yValue = controller.getRawAxis(XBoxControllerMap.LeftJoystickY);
-        if(Math.abs(yValue) > dead_zone){
+        if(Math.abs(yValue) > deadZone){
             return yValue;
         }
         return 0;
     }
 
-    public static double leftJoystickX(Joystick controller, double dead_zone){
+    public static double leftJoystickX(Joystick controller, double deadZone){
         double xValue = controller.getRawAxis(XBoxControllerMap.LeftJoystickX);
-        if(Math.abs(xValue) > dead_zone){
+        if(Math.abs(xValue) > deadZone){
             return xValue;
         }
         return 0;
     }
 
-    public static double rightJoystickY(Joystick controller, double dead_zone){
+    public static double rightJoystickY(Joystick controller, double deadZone){
         double yValue = controller.getRawAxis(XBoxControllerMap.RightJoystickY);
-        if(Math.abs(yValue) > dead_zone){
+        if(Math.abs(yValue) > deadZone){
             return yValue;
         }
         return 0;
     }
 
-    public static double rightJoystickX(Joystick controller, double dead_zone){
+    public static double rightJoystickX(Joystick controller, double deadZone){
         double xValue = controller.getRawAxis(XBoxControllerMap.RightJoystickX);
-        if(Math.abs(xValue) > dead_zone){
+        if(Math.abs(xValue) > deadZone){
             return xValue;
         }
         return 0;
