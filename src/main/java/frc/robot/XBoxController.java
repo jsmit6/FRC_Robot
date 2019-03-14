8ed6953642664f2f36882fb3db804ff608d07991
Brian-Thomas Rogers <broge2@uis.edu>
2019-03-14 01:41:36 -0500

    Add support for RT and LT

--- src/main/java/frc/robot/XBoxController.java
+++ src/main/java/frc/robot/XBoxController.java
@@ -4,35 +4,38 @@ import edu.wpi.first.wpilibj.Joystick;
 import frc.robot.XBoxControllerMap;
 
 public class XBoxController {
-    public static double leftJoystickY(Joystick controller, double deadZone){
+
+    public static double leftJoystickY(Joystick controller, double deadZone) {
         double yValue = controller.getRawAxis(XBoxControllerMap.LeftJoystickY);
-        if(Math.abs(yValue) > deadZone){
-            return yValue;
-        }
-        return 0;
+        return deadZoneCheck(yValue, deadZone);
     }
 
-    public static double leftJoystickX(Joystick controller, double deadZone){
+    public static double leftJoystickX(Joystick controller, double deadZone) {
         double xValue = controller.getRawAxis(XBoxControllerMap.LeftJoystickX);
-        if(Math.abs(xValue) > deadZone){
-            return xValue;
-        }
-        return 0;
+        return deadZoneCheck(xValue, deadZone);
     }
 
-    public static double rightJoystickY(Joystick controller, double deadZone){
+    public static double rightJoystickY(Joystick controller, double deadZone) {
         double yValue = controller.getRawAxis(XBoxControllerMap.RightJoystickY);
-        if(Math.abs(yValue) > deadZone){
-            return yValue;
-        }
-        return 0;
+        return deadZoneCheck(yValue, deadZone);
     }
 
-    public static double rightJoystickX(Joystick controller, double deadZone){
+    public static double rightJoystickX(Joystick controller, double deadZone) {
         double xValue = controller.getRawAxis(XBoxControllerMap.RightJoystickX);
-        if(Math.abs(xValue) > deadZone){
-            return xValue;
-        }
-        return 0;
+        return deadZoneCheck(xValue, deadZone);
+    }
+
+    public static double RT(Joystick controller, double deadZone) {
+        double rtValue = controller.getRawAxis(XBoxControllerMap.RT);
+        return deadZoneCheck(rtValue, deadZone);
+    }
+
+    public static double LT(Joystick controller, double deadZone) {
+        double ltValue = controller.getRawAxis(XBoxControllerMap.LT);
+        return deadZoneCheck(ltValue, deadZone);
+    }
+
+    private static double deadZoneCheck(double value, double deadZone) {
+        return Math.abs(value) > deadZone ? value : 0;
     }
 }
