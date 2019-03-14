b7517b1921f4fdacf6e12272a9d13fab03639be3
Brian-Thomas Rogers <broge2@uis.edu>
2019-03-14 01:15:43 -0500

    Use XBoxController class to keep deadzone check logic the same

--- src/main/java/frc/robot/commands/Wrist.java
+++ src/main/java/frc/robot/commands/Wrist.java
@@ -6,11 +6,12 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.OI;
 import frc.robot.Robot;
+import frc.robot.XBoxController;
 
 public class Wrist extends Command {
 
     private Joystick controller = OI.xboxController;
-    private double threshold = 0.05;
+    private double deadZone = 0.05;
 
     public Wrist() {
         requires(Robot.wristSubsystem);
@@ -25,20 +26,14 @@ public class Wrist extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
-        double yAxis = controller.getRawAxis(RightJoystickY);
-
-        if(Math.abs(yAxis) > threshold){
-            Robot.wristSubsystem.rotate(-yAxis / 2);
-        }
+        double yAxis = XBoxController.rightJoystickY(controller, deadZone);
+        Robot.wristSubsystem.rotate(-yAxis / 2);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     @Override
     protected boolean isFinished() {
-        if(Math.abs(controller.getRawAxis(RightJoystickY)) < threshold){
-                return true;
-        }
-        return false;
+        return XBoxController.rightJoystickY(controller, deadZone) == 0;
     }
 
     // Called once after isFinished returns true
