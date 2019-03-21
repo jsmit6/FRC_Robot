715f05137ace9b3ada0b75f2b09666e85cbe570e
Brian-Thomas Rogers <broge2@uis.edu>
2019-03-21 10:22:14 -0500

    Add extra check for engaged brake

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -28,7 +28,7 @@ public class ArmSubsystem extends Subsystem {
 
     public ArmSubsystem(boolean enabled) {
         ENABLED = enabled;
-        if(enabled){
+        if (enabled) {
             liftMotor = new CANSparkMax(armMotorID, MotorType.kBrushless);
             liftMotor.setSmartCurrentLimit(Constants.VOLTAGE_LIMIT);
             liftMotor.setInverted(false);
@@ -41,49 +41,52 @@ public class ArmSubsystem extends Subsystem {
 
     @Override
     public void initDefaultCommand() {
-        if(ENABLED){
+        if (ENABLED) {
             setDefaultCommand(new Arm());
         }
     }
 
     @Override
     public void periodic() {
-       SmartDashboard.putNumber("Arm Potentiometer", getPotValue());
+        SmartDashboard.putNumber("Arm Potentiometer", getPotValue());
     }
 
-    private int getPotValue(){
+    private int getPotValue() {
         return (int) (pot.get() * 100);
     }
 
-    private void unbrake(){
+    private void unbrake() {
         brakeSol.set(Value.kReverse);
         brakeEngaged = false;
     }
 
-    private void brake(){
+    private void brake() {
         brakeSol.set(Value.kForward);
         brakeEngaged = true;
     }
 
-    public void lift(double lTrigger, double rTrigger, boolean override){
+    public void lift(double lTrigger, double rTrigger, boolean override) {
         int potValue = getPotValue();
         SmartDashboard.putBoolean("Arm Override", override);
         WristSubsystem wrist = Robot.wristSubsystem;
-        if(brakeEngaged){
+        if (brakeEngaged) {
             unbrake();
             Timer.delay(0.2);
         }
-        if(potValue <= Constants.MIN_ARM_LOWER_POT && override && potValue >= Constants.OVERRIDE_MAX_ARM_RAISE_POT){
-            wrist.rotateToPosition(Constants.WRIST_MIN_LIMIT);
-            liftMotor.set(lTrigger);
-        }else if(potValue >= Constants.MIN_ARM_LOWER_POT){
-            liftMotor.set(-rTrigger);
-        }else{
-            liftMotor.set((lTrigger - rTrigger));
+        if (!brakeEngaged) {
+            if (potValue <= Constants.MIN_ARM_LOWER_POT && override
+                    && potValue >= Constants.OVERRIDE_MAX_ARM_RAISE_POT) {
+                wrist.rotateToPosition(Constants.WRIST_MIN_LIMIT);
+                liftMotor.set(lTrigger);
+            } else if (potValue >= Constants.MIN_ARM_LOWER_POT) {
+                liftMotor.set(-rTrigger);
+            } else {
+                liftMotor.set((lTrigger - rTrigger));
+            }
         }
     }
 
-    public void stop(){
+    public void stop() {
         liftMotor.stopMotor();
         brake();
     }
