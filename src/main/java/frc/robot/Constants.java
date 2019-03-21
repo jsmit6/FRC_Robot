3eb5f888ad611cedace85a8658a307f3b419637e
Brian-Thomas Rogers <broge2@uis.edu>
2019-03-20 22:24:15 -0500

    Set open loop ramp rate code there but NA

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,4 +13,6 @@ public class Constants{
     public static final int MIN_ARM_LOWER_POT = 31;
     public static final int OVERRIDE_MAX_ARM_RAISE_POT = 19;
 
+    public static final int RAMP_UP_VALUE = 0;
+
 }
